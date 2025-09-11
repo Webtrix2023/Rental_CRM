343899e07d581d381760b042ed759d62ab7a9823
aniketsonawane <aniketcsonawane@gmail.com>
2025-09-11 10:51:43 +0530

    Aniket :  Gst added in Rental invoice

--- src/custom/ComputerRental/Invoice/ProductInvocing.jsx
+++ src/custom/ComputerRental/Invoice/ProductInvocing.jsx
@@ -40,9 +40,10 @@ export default function ProductInvocing() {
     billingDate: new Date().toISOString().substring(0, 10),
     billingPeriod: "Monthly",
     is_gst_billing: "no",
-    centralGst: '9',
-    stateGst: '9',
-    interGst: '18',
+    customGst: '0',
+    centralGst: '0',
+    stateGst: '0',
+    interGst: '0',
     company_state_id: '0',
     summary: {},
     selectedAssets: [],
@@ -80,16 +81,16 @@ export default function ProductInvocing() {
   };
 
   function isLastBillDatePassed(lastBillDateStr) {
-  if (!lastBillDateStr) return false; // handle empty/null
+    if (!lastBillDateStr) return false; // handle empty/null
 
-  const today = new Date();
-  today.setHours(0, 0, 0, 0); // normalize today
+    const today = new Date();
+    today.setHours(0, 0, 0, 0); // normalize today
 
-  const lastBillDate = new Date(lastBillDateStr);
-  lastBillDate.setHours(0, 0, 0, 0); // normalize input
+    const lastBillDate = new Date(lastBillDateStr);
+    lastBillDate.setHours(0, 0, 0, 0); // normalize input
 
-  return lastBillDate < today; // true if passed
-}
+    return lastBillDate < today; // true if passed
+  }
 
   const canBillReturnedProduct = (invoiceDate, lastBillDate, returnDate, billing_type) => {
     const date1 = new Date(invoiceDate);
@@ -143,15 +144,15 @@ export default function ProductInvocing() {
     // daily/other billing types
     return date1 > date2;
   };
-  const isDisabled = (a)=> {
+  const isDisabled = (a) => {
     // invoiceDetails.billingDate = "2025-11-04";
     console.log('a :', a);
-    if (a.row_close === "y" && !canBillReturnedProduct(invoiceDetails.billingDate,a.last_bill_date,a.returnDate, a.billing_type)) {
+    if (a.row_close === "y" && !canBillReturnedProduct(invoiceDetails.billingDate, a.last_bill_date, a.returnDate, a.billing_type)) {
       return true;
-    }else if (a.row_close === "n" && !compareDate(invoiceDetails.billingDate, a.last_bill_date, a.billing_type)){
+    } else if (a.row_close === "n" && !compareDate(invoiceDetails.billingDate, a.last_bill_date, a.billing_type)) {
       return true;
     }
-    return false;   
+    return false;
   }
 
   const getDeliveries = () => {
@@ -215,10 +216,6 @@ export default function ProductInvocing() {
       const data = res.data[0] ?? {};
       setInvoiceDetails((prev) => ({
         ...prev,
-        "is_gst_billing": data.is_gst_billing ?? 'no',
-        "centralGst": data.centralGst ?? '9',
-        "stateGst": data.centralGst ?? '9',
-        "interGst": data.interGst ?? '18',
         "company_state_id": data.state ?? '0'
       }));
     }).finally(() => setLoading(false));
@@ -256,11 +253,16 @@ export default function ProductInvocing() {
   let cgst = 0, sgst = 0, igst = 0;
 
   if (invoiceDetails.is_gst_billing === "yes") {
-    if (
-      customer?.gst_state &&
-      invoiceDetails.company_state_id &&
-      customer.gst_state.toString() !== invoiceDetails.company_state_id.toString()
-    ) {
+    if (customer?.gst_state && invoiceDetails.company_state_id && customer.gst_state.toString() !== invoiceDetails.company_state_id.toString()){
+      invoiceDetails.stateGst = 0; 
+      invoiceDetails.centralGst = 0; 
+      invoiceDetails.interGst = invoiceDetails.customGst; 
+    }else{
+      invoiceDetails.interGst = 0; 
+      invoiceDetails.stateGst = (invoiceDetails.customGst) ? (invoiceDetails.customGst / 2) : 0; 
+      invoiceDetails.centralGst = (invoiceDetails.customGst) ? (invoiceDetails.customGst / 2) : 0; 
+    } 
+    if (customer?.gst_state && invoiceDetails.company_state_id && customer.gst_state.toString() !== invoiceDetails.company_state_id.toString()) {
       // Interstate → IGST
       igst = Math.round(
         subtotal * (Number(invoiceDetails.interGst || 0) / 100)
@@ -275,8 +277,15 @@ export default function ProductInvocing() {
       );
     }
   }
+  // if (invoiceDetails.is_gst_billing === "yes") {
+  //   // Interstate → IGST
+  //   interGstAmount = Math.round(
+  //     subtotal * (Number(invoiceDetails.interGstPercent || 0) / 100)
+  //   );
+  // }
 
-  const gst = cgst + sgst + igst;
+  const gst = cgst + sgst + igst ;
+
   const totalAmount = subtotal + gst;
 
   const handleGenerateInvoice = async () => {
@@ -409,7 +418,7 @@ export default function ProductInvocing() {
             <button
               onClick={selectAll}
               className={` mr-4 hover:underline ${assets.length == 0 ? 'text-gray-500' : 'text-blue-500'}`}
-              disabled={assets.length == 0} 
+              disabled={assets.length == 0}
             >
               Select All
             </button>
@@ -435,9 +444,7 @@ export default function ProductInvocing() {
         ) : (
           assets.map((a) => {
             const disabled = (a.revenue == 0) || isDisabled(a);
-
-            console.log('disabled : ',disabled);
-            
+            console.log('disabled : ', disabled);
             return (
               <div
                 key={a.itemID}
@@ -494,18 +501,18 @@ export default function ProductInvocing() {
                       {a.is_replacement == 'y' && (<><span className="mx-1 text-green-900">REPL</span><span>| </span></>)}
                       {a.upgraded_bill == 'Y' && a.upgraded_rate && (
                         <>
-                        <span className="text-red-400">
-                          Upgraded Rate ₹{a.upgraded_rate}
-                        </span> 
-                        <span className="mx-1"> |</span>                    
+                          <span className="text-red-400">
+                            Upgraded Rate ₹{a.upgraded_rate}
+                          </span>
+                          <span className="mx-1"> |</span>
                         </>
                       )}
                       <span className="">
                         Rate: ₹{a.invoiceLineRate}/month
-                      </span> 
+                      </span>
                     </div>
                     <div className="text-green-600 font-bold">
-                      
+
                       <span>
                         ₹{a.revenue}
                       </span>
@@ -520,7 +527,7 @@ export default function ProductInvocing() {
                   </div>
                 </div>
                 <div className="mt-2 pt-1 text-sm text-gray-500 flex items-center">
-                  {a.replaced_revenue && Object.entries(a.replaced_revenue).length != 0  && (
+                  {a.replaced_revenue && Object.entries(a.replaced_revenue).length != 0 && (
                     <>
                       <span>Replace Items Note: {!a.replaced_revenue && (<span>N/A</span>)} </span>
                       {a.replaced_revenue && (
@@ -538,7 +545,7 @@ export default function ProductInvocing() {
                           )}
                           {a.replaced_revenue?.start && a.replaced_revenue?.end && (
                             <span className="mx-1">
-                              Billing Period : { a.replaced_revenue?.start } to { a.replaced_revenue?.end } 
+                              Billing Period : {a.replaced_revenue?.start} to {a.replaced_revenue?.end}
                             </span>
                           )}
                         </>
@@ -565,28 +572,57 @@ export default function ProductInvocing() {
             <div className="text-xs mb-1 flex justify-between">
               Subtotal: <span className="text-gray-700">₹{subtotal}</span>
             </div>
+            <div className="flex items-center mb-3">
+              <input
+                type="checkbox"
+                id="is_gst_billing"
+                checked={invoiceDetails.is_gst_billing === "yes" ? true : false}
+                onChange={(e) => handleInputChange("is_gst_billing", e.target.checked ? 'yes' : 'no')}
+                className="mr-2"
+              />
+              <label htmlFor="is_gst_billing" className="text-sm text-gray-700">
+                Include GST
+              </label>
+              {invoiceDetails.is_gst_billing == 'yes' && (
+                <div className="flex-1 min-w-[50px] max-w-[50px] mb-1 ml-3">
+                  {/* <label className="text-sm block text-gray-700">Enter GST</label> */}
+                  <input
+                    id="customGst"
+                    type="text"
+                    value={invoiceDetails.customGst}
+                    placeholder="GST"
+                    onChange={(e) => handleInputChange("customGst", e.target.value)}
+                    className="w-20 border border-gray-300 rounded px-2 py-1 pr-10 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  />
+                </div>
+              )}
+            </div>
             {invoiceDetails.is_gst_billing === "yes" ? (
               <>
-                {cgst > 0 && (
-                  <div className="text-xs mb-1 flex justify-between">
-                    CGST ({invoiceDetails.centralGst}%):
-                    <span className="text-gray-700">₹{cgst}</span>
-                  </div>
-                )}
-                {sgst > 0 && (
-                  <div className="text-xs mb-1 flex justify-between">
-                    SGST ({invoiceDetails.stateGst}%):
-                    <span className="text-gray-700">₹{sgst}</span>
-                  </div>
-                )}
-                {igst > 0 && (
-                  <div className="text-xs mb-1 flex justify-between">
-                    IGST ({invoiceDetails.interGst}%):
-                    <span className="text-gray-700">₹{igst}</span>
-                  </div>
-                )}
-              </>
+              {cgst > 0 && (
+                <div className="text-xs mb-1 flex justify-between">
+                  CGST ({invoiceDetails.centralGst}%):
+                  <span className="text-gray-700">₹{cgst}</span>
+                </div>
+              )}
+              {sgst > 0 && (
+                <div className="text-xs mb-1 flex justify-between">
+                  SGST ({invoiceDetails.stateGst}%):
+                  <span className="text-gray-700">₹{sgst}</span>
+                </div>
+              )}
+              {igst > 0 && (
+                <div className="text-xs mb-1 flex justify-between">
+                  IGST ({invoiceDetails.interGst}%):
+                  <span className="text-gray-700">₹{igst}</span>
+                </div>
+              )}
+            </>
             ) : ''}
+            {/* <div className="text-xs mb-1 flex justify-between">
+              GST ({invoiceDetails.interGstPercent}%):
+              <span className="text-gray-700">₹{interGstAmount}</span>
+            </div> */}
             <div className="text-lg font-bold mt-2 flex justify-between">
               Total Amount:{" "}
               <span className="text-green-600">₹{totalAmount}</span>
